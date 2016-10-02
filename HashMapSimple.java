7e9763967ee68977f105424a0bec0e9d1295450f
mydoum <a.alonso.giraldo@gmail.com>
2016-10-02 20:30:48 +0200

    Managing collisions

--- HashMapSimple.java
+++ HashMapSimple.java
@@ -1,9 +1,11 @@
 package pack;
 
 import java.util.LinkedList;
+import java.util.Map;
 
 public class HashMapSimple<K, V> {
-    private LinkedList<V>[] arrLinkList;
+    private LinkedList<Entry<K, V>>[] arrLinkList;
+    private int size = 0;
     /**
      * The capacity should be a prime number in order to avoid extra collisions
      */
@@ -15,6 +17,7 @@ public class HashMapSimple<K, V> {
 
     HashMapSimple(int capacity) {
         arrLinkList = new LinkedList[capacity];
+
     }
 
     /**
@@ -27,16 +30,65 @@ public class HashMapSimple<K, V> {
      * @return
      */
     private int hashFunction(K key) {
-        return key.hashCode() % arrLinkList.length;
+        return key.hashCode() % arrLinkList.length - 1;
     }
 
     public void put(K key, V value) {
-        LinkedList<V> l = new LinkedList<>();
-        l.add(value);
-        arrLinkList[hashFunction(key)] = l;
+        int hashResult = hashFunction(key);
+        boolean inserted = false;
+        if (arrLinkList[hashResult] == null) {
+            arrLinkList[hashResult] = new LinkedList<>();
+        }
+
+        for (int i = 0; i < arrLinkList[hashResult].size(); i++) {
+            if (arrLinkList[hashResult].get(i).getKey() == key) {
+                arrLinkList[hashResult].get(i).setValue(value);
+                inserted = true;
+            }
+        }
+
+        if (!inserted) {
+            arrLinkList[hashResult].add(new Entry<>(key, value));
+            size++;
+        }
     }
 
     public V get(K key) {
-        return arrLinkList[hashFunction(key)].getFirst();
+        int listSize = arrLinkList[hashFunction(key)].size();
+        for (int i = 0; i < listSize; i++) {
+            if (arrLinkList[hashFunction(key)].get(i).getKey() == key)
+                return arrLinkList[hashFunction(key)].get(i).getValue();
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    class Entry<K,V> implements Map.Entry<K,V> {
+        private final K key;
+        private V value;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return this.key;
+        }
+
+        @Override
+        public V getValue() {
+            return this.value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            this.value = value;
+            return null;
+        }
     }
 }
--- HashMapSimpleTest.java
+++ HashMapSimpleTest.java
@@ -15,14 +15,40 @@ public class HashMapSimpleTest {
         myHash = new HashMapSimple<>();
     }
 
+    /**
+     *
+     * Method: put(K key, V value)
+     *
+     */
     @Test
     public void testPut() throws Exception {
         myHash.put("hello", 3);
         assertEquals(3, (int)myHash.get("hello"));
     }
 
+    /**
+     *
+     * Method: get(K key)
+     *
+     */
     @Test
     public void testGet() throws Exception {
+        myHash.put("FB", 28);
+        myHash.put("Ea", 54);
+        assertEquals(54, (int)myHash.get("Ea"));
+    }
 
+    /**
+     *
+     * Method: size()
+     *
+     */
+    @Test
+    public void testSize() throws Exception {
+        assertEquals(0, myHash.size());
+        myHash.put("hello", 3);
+        assertEquals(1, myHash.size());
+        myHash.put("hello", 5);
+        assertEquals(1, myHash.size());
     }
 }
